331c0cf3420de298b3b0086d277bee572e8d5a58
Alessia Nigretti <alessia@klang-games.com>
2020-07-16 11:25:23 +0200

    Intersection points

--- Assets/Scripts/AgentSpawner.cs
+++ Assets/Scripts/AgentSpawner.cs
@@ -13,14 +13,12 @@ public class AgentSpawner : MonoBehaviour
     private NavMeshAgent mainAgent;
     private List<NavMeshAgent> agents = new List<NavMeshAgent>();
     private RandomMovement randomMovement;
-    private SteeringBehaviour steeringBehaviour;
     private KDTreeBuilder kdTree;
     
-    void Start()
+    void OnEnable()
     {
         kdTree = new KDTreeBuilder();
         randomMovement = new RandomMovement();
-        steeringBehaviour = new SteeringBehaviour();
         mainAgent = GameObject.Find("Agent").GetComponent<NavMeshAgent>();
         
         for (int i = 0; i < amount; i++)
@@ -32,29 +30,16 @@ public class AgentSpawner : MonoBehaviour
 
     void Update()
     {
-        var tree = kdTree.BuildKDTree(agents);//GetPoints());
-        var nearestNeighbours = kdTree.NearestNeighbours(nNearest, tree, mainAgent);//new Point(mainAgent.transform.position.x, mainAgent.transform.position.z));
-        foreach (var point in nearestNeighbours)
+        var tree = kdTree.BuildKDTree(agents);
+        //var nearestNeighbours = kdTree.NearestNeighbours(nNearest, tree, mainAgent);
+        List<KDTreeBuilder.Neighbour> nearestNeighbours = new List<KDTreeBuilder.Neighbour>();
+        foreach (var agent in agents)
         {
-            var position = new Vector3(point.Agent.transform.position.x, 0, point.Agent.transform.position.y);
-            if (Input.GetKeyDown(KeyCode.N))
+            nearestNeighbours.Add(new KDTreeBuilder.Neighbour
             {
-                Instantiate(highlightPrefab, position, highlightPrefab.transform.rotation);
-            }
+                Agent = agent
+            });
         }
-        var constraints = steeringBehaviour.ComputeFVOConstraints(mainAgent, nearestNeighbours);
+        //var constraints = steeringBehaviour.ComputeFVOConstraints(nearestNeighbours);
     }
-
-    // private List<Point> GetPoints()
-    // {
-    //     List<Point> points = new List<Point>();
-    //     
-    //     foreach (var agent in agents)
-    //     {
-    //         var position = agent.transform.position;
-    //         points.Add(new Point(position.x, position.z));
-    //     }
-    //
-    //     return points;
-    // }
 }
--- Assets/Scripts/MovementAlgorithms/SteeringBehaviour.cs
+++ Assets/Scripts/MovementAlgorithms/SteeringBehaviour.cs
@@ -1,50 +1,124 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
-public struct FVOConstraint
+public class SteeringBehaviour : MonoBehaviour
 {
-    public Ray LeftRayInside;
-    public Ray LeftRayOutside;
-    public Ray RightRayInside;
-    public Ray RightRayOutside;
-}
+    private NavMeshAgent navMeshAgent;
+    //private float rayDistance = 10f;
+    //private List<FVOConstraint> constraints = new List<FVOConstraint>();
 
-public class SteeringBehaviour
-{
-    private float rayDistance = 10f;
-    private List<FVOConstraint> constraints = new List<FVOConstraint>();
-    
-    public List<FVOConstraint> ComputeFVOConstraints(NavMeshAgent agent, List<KDTreeBuilder.Neighbour> obstaclesPos)
+    //private GameObject colliderRightIn, colliderRightOut, colliderLeftIn, colliderLeftOut;
+
+    // public SteeringBehaviour()
+    // {
+    //     colliderRightIn = new GameObject("RightIn");
+    //     colliderRightIn.AddComponent<EdgeCollider2D>();
+    //     colliderRightOut = new GameObject("RightOut");
+    //     colliderRightOut.AddComponent<EdgeCollider2D>();
+    //     colliderLeftIn = new GameObject("LeftIn");
+    //     colliderLeftIn.AddComponent<EdgeCollider2D>();
+    //     colliderLeftOut = new GameObject("LeftOut");
+    //     colliderLeftOut.AddComponent<EdgeCollider2D>();
+    // }
+    //
+    // public List<FVOConstraint> ComputeFVOConstraints(List<KDTreeBuilder.Neighbour> obstaclesPos)
+    // {
+    //     constraints.Clear();
+    //     
+    //     foreach (var obstacle in obstaclesPos)
+    //     {
+    //         var agentTransform = obstacle.Agent.transform;
+    //
+    //         colliderRightIn.transform.position = agentTransform.position + agentTransform.forward * 0.25f;
+    //         RaycastHit2D[] resultsRightIn = new RaycastHit2D[2];
+    //         colliderRightIn.GetComponent<EdgeCollider2D>().Raycast((agentTransform.forward + agentTransform.right * obstacle.Agent.radius).normalized, resultsRightIn, rayDistance - 0.25f);
+    //         foreach (var result in resultsRightIn)
+    //         {
+    //             if (result.transform != null && result.transform.gameObject.name != "RightIn")
+    //             {
+    //                 Debug.Log(result.transform.gameObject.name);
+    //             }
+    //         }
+    //         
+    //         colliderRightOut.transform.position = agentTransform.position;
+    //         RaycastHit2D[] resultsRightOut = new RaycastHit2D[2];
+    //         colliderRightOut.GetComponent<EdgeCollider2D>().Raycast((agentTransform.forward + agentTransform.right * obstacle.Agent.radius).normalized, resultsRightOut, rayDistance);
+    //         foreach (var result in resultsRightOut)
+    //         {
+    //             if (result.transform != null && result.transform.gameObject.name != "RightOut")
+    //             {
+    //                 Debug.Log(result.transform.gameObject.name);
+    //             }
+    //         }
+    //         
+    //         colliderLeftIn.transform.position = agentTransform.position + agentTransform.forward * 0.25f;
+    //         RaycastHit2D[] resultsLeftIn = new RaycastHit2D[2];
+    //         colliderLeftIn.GetComponent<EdgeCollider2D>().Raycast((agentTransform.forward - agentTransform.right * obstacle.Agent.radius).normalized, resultsLeftIn, rayDistance - 0.25f);
+    //         foreach (var result in resultsLeftIn)
+    //         {
+    //             if (result.transform != null && result.transform.gameObject.name != "LeftIn")
+    //             {
+    //                 Debug.Log(result.transform.gameObject.name);
+    //             }
+    //         }
+    //         
+    //         colliderLeftOut.transform.position = agentTransform.position;
+    //         RaycastHit2D[] resultsLeftOut = new RaycastHit2D[2];
+    //         colliderLeftOut.GetComponent<EdgeCollider2D>().Raycast((agentTransform.forward - agentTransform.right * obstacle.Agent.radius).normalized, resultsLeftOut, rayDistance);
+    //         foreach (var result in resultsLeftOut)
+    //         {
+    //             if (result.transform != null && result.transform.gameObject.name != "LeftOut")
+    //             {
+    //                 Debug.Log(result.transform.gameObject.name);
+    //             }
+    //         }
+    //         
+    //         var rayObstacleRightInside = new Ray(agentTransform.position + agentTransform.forward * 0.25f, (agentTransform.forward + agentTransform.right * obstacle.Agent.radius).normalized);
+    //         var rayObstacleRightOutside = new Ray(agentTransform.position, (agentTransform.forward + agentTransform.right * obstacle.Agent.radius).normalized);
+    //         var rayObstacleLeftInside = new Ray(agentTransform.position + agentTransform.forward * 0.25f, (agentTransform.forward - agentTransform.right * obstacle.Agent.radius).normalized);
+    //         var rayObstacleLeftOutside = new Ray(agentTransform.position, (agentTransform.forward - agentTransform.right * obstacle.Agent.radius).normalized);
+    //         
+    //         Debug.DrawRay(rayObstacleRightInside.origin, rayObstacleRightInside.direction * (rayDistance - 0.25f), Color.red);
+    //         Debug.DrawRay(rayObstacleLeftInside.origin, rayObstacleLeftInside.direction * (rayDistance - 0.25f), Color.red);
+    //         Debug.DrawRay(rayObstacleRightOutside.origin, rayObstacleRightOutside.direction * rayDistance, Color.blue);
+    //         Debug.DrawRay(rayObstacleLeftOutside.origin, rayObstacleLeftOutside.direction * rayDistance, Color.blue);
+    //         
+    //         constraints.Add(new FVOConstraint
+    //         {
+    //             RightRayInside = rayObstacleRightInside,
+    //             RightRayOutside = rayObstacleRightOutside,
+    //             LeftRayInside = rayObstacleLeftInside,
+    //             LeftRayOutside = rayObstacleLeftOutside
+    //         });
+    //     }
+    //
+    //     return constraints;
+    // }
+
+    private void Start()
     {
-        constraints.Clear();
-        
-        var rayAgentRight = new Ray(agent.transform.position, (agent.transform.forward + agent.transform.right * agent.radius).normalized);
-        var rayAgentLeft = new Ray(agent.transform.position, (agent.transform.forward - agent.transform.right * agent.radius).normalized);
-        Debug.DrawRay(rayAgentRight.origin, rayAgentRight.direction * rayDistance, Color.red);
-        Debug.DrawRay(rayAgentLeft.origin, rayAgentLeft.direction * rayDistance, Color.blue);
-        
-        foreach (var obstacle in obstaclesPos)
+        navMeshAgent = GetComponentInParent<NavMeshAgent>();
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Untagged"))
         {
-            var agentTransform = obstacle.Agent.transform;
-            var rayObstacleRightInside = new Ray(agentTransform.position + agentTransform.forward * 0.25f, (agentTransform.forward + agentTransform.right * agent.radius).normalized);
-            var rayObstacleRightOutside = new Ray(agentTransform.position, (agentTransform.forward + agentTransform.right * agent.radius).normalized);
-            var rayObstacleLeftInside = new Ray(agentTransform.position + agentTransform.forward * 0.25f, (agentTransform.forward - agentTransform.right * agent.radius).normalized);
-            var rayObstacleLeftOutside = new Ray(agentTransform.position, (agentTransform.forward - agentTransform.right * agent.radius).normalized);
-            Debug.DrawRay(rayObstacleRightInside.origin, rayObstacleRightInside.direction * rayDistance, Color.red);
-            Debug.DrawRay(rayObstacleLeftInside.origin, rayObstacleLeftInside.direction * rayDistance, Color.red);
-            Debug.DrawRay(rayObstacleRightOutside.origin, rayObstacleRightOutside.direction * rayDistance, Color.blue);
-            Debug.DrawRay(rayObstacleLeftOutside.origin, rayObstacleLeftOutside.direction * rayDistance, Color.blue);
-
-            constraints.Add(new FVOConstraint
-            {
-                RightRayInside = rayObstacleRightInside,
-                RightRayOutside = rayObstacleRightOutside,
-                LeftRayInside = rayObstacleLeftInside,
-                LeftRayOutside = rayObstacleLeftOutside
-            });
+            return;
         }
+        
+        switch (other.gameObject.tag)
+        {
+            case "Outside":
+                // TODO: sort by contact point distance from x0
+                var closestOutsidePoint = other.contacts[0].point;
 
-        return constraints;
+                Debug.Log("Assigning new velocity");
+                navMeshAgent.velocity = closestOutsidePoint;
+                
+                break;
+            case "Inside":
+                break;
+        }
     }
 }
